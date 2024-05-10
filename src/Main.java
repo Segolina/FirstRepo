cdfa00b6a87eafe114272244c193a2da456fa2cb
Segolina <222332255@edu.vut.ac.za>
2024-05-10 14:50:10 +0200

    primo

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(Systems.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("You're home");
         String name = input.next();
         System.out.println("Welcome" + name);
