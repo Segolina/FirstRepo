fbd5927752b80b5cd883727e5d1dd4fcb6fee45e
Segolina <222332255@edu.vut.ac.za>
2024-05-10 13:56:25 +0200

    First change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(Systems.in)
+        System.out.print("You're home");
+        String name = input.next();
+        System.out.println("Welcome" + name);
     }
 }
